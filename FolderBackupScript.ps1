c31d81fdd3cd5f29ea440fe95cf0825ac77ccd2b
Manos <igoufe.osman@gmail.com>
2024-03-05 22:22:46 -0500

    first commit

---
+++ FolderBackupScript.ps1
@@ -0,0 +1,7 @@
+Param(
+    [string]$Path = './app',
+    [string]$DestinationPath = './'
+)
+$date = Get-Date -format "yyyy-MM-dd"
+Compress-Archive -Path $Path -CompressionLevel 'Fastest' -DestinationPath "./backup-$date"
+Write-Host "Created backup at $( './backup-' + $date + '.zip' )"
